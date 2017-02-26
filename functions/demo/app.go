da96b5a85afef6552bd1f5c11367ce8c740d8086
Kentaro Kawano <kentaro.kawano@gmail.com>
2017-02-26 10:58:12 +0900

    init apex project

---
+++ functions/demo/app.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/fujiwara/ridge"
+	"github.com/kawaken/rod"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	logger := log.New(os.Stderr, "", log.Lshortfile)
+
+	handler, err := rod.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
+	if err != nil {
+		logger.Print(err)
+		return
+	}
+
+	handler.OnTextMessageRecieved = func(e *linebot.Event, m *linebot.TextMessage) []linebot.Message {
+		return []linebot.Message{
+			&linebot.TextMessage{Text: m.Text},
+		}
+	}
+
+	ridge.Run(":8080", "/callback", handler)
+}
